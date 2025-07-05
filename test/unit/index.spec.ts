8b979106a79ab8e2960b695f26fdab12db25f12c
PhilLavoie <PhilLavoie@users.noreply.github.com>
2025-07-05 06:14:24 -0400

    stubbed integration tests and reviewed the default lib impl

--- test/unit/index.spec.ts
+++ test/unit/index.spec.ts
@@ -1,10 +1,9 @@
 import { expect } from "@infra-blocks/test";
-import { testMe } from "../../src/index.js";
 
 describe("index", () => {
-  describe(testMe.name, () => {
+  describe("dummy test", () => {
     it("should return the right stuff", () => {
-      expect(testMe()).to.equal("you tested me all right!");
+      expect(true).to.be.true;
     });
   });
 });
