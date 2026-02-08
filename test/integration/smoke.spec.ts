c05ff41b5bb736b5e99d03d0ec7757a91f2a4dcd
PhilLavoie <PhilLavoie@users.noreply.github.com>
2026-02-08 09:03:40 -0500

    use node.js test runner

--- test/integration/smoke.spec.ts
+++
@@ -1,7 +0,0 @@
-import { expect } from "@infra-blocks/test";
-
-describe("dummy smoke tests", () => {
-  it("should work, yo", () => {
-    expect(true).to.be.true;
-  });
-});
--- test/unit/index.spec.ts
+++
@@ -1,9 +0,0 @@
-import { expect } from "@infra-blocks/test";
-
-describe("index", () => {
-  describe("dummy test", () => {
-    it("should return the right stuff", () => {
-      expect(true).to.be.true;
-    });
-  });
-});
